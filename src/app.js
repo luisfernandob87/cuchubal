3ef1b2c2c29a4e285a2569b3a772e2869c5da1b0
luis.morales <luisfernandob87@gmail.com>
2023-06-23 19:06:38 -0600

    add jwt

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require("express")
 const { routerUser } = require('./routes/usuario.routes')
 const { routerCuchubal } = require('./routes/cuchubal.routes')
 const { routerUsuarioCuota } = require('./routes/usuarioCuota.routes')
+const { routerAut } = require('./controllers/auth.controller')
 
 
 const app = express()
 
 //middleware
 app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(routerAut)
 
 app.use(routerUser)
 
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,11 +1,9 @@
 const { Router } = require('express')
-const { createUsuario, getUsuarios, updateUsuario, deleteUsuario, getUsuario, getUsuarioCuotas } = require('../controllers/usuarios.controller')
+const { getUsuarios, deleteUsuario, getUsuario, getUsuarioCuotas } = require('../controllers/usuarios.controller')
 
 const routerUser = Router()
 
 routerUser.get('/usuarios', getUsuarios)
-routerUser.post('/usuario', createUsuario)
-routerUser.put('/usuario/:id', updateUsuario)
 routerUser.delete('/usuario/:id', deleteUsuario)
 routerUser.get('/usuario/:id', getUsuario)
 
