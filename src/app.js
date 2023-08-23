b32aff07d3dfb0cb0e634f743443c8b6ac116cde
luis.morales <luisfernandob87@gmail.com>
2023-08-23 17:31:18 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const { routerUser } = require('./routes/usuario.routes')
 const { routerCuchubal } = require('./routes/cuchubal.routes')
 const { routerUsuarioCuota } = require('./routes/usuarioCuota.routes')
 const { routerAut } = require('./controllers/auth.controller')
-
+const cors = require("cors");
 
 const app = express()
 
 //middleware
+app.use(cors());
+
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
@@ -19,5 +21,6 @@ app.use(routerUser)
 app.use(routerCuchubal)
 
 app.use(routerUsuarioCuota)
+
 
 module.exports = { app }
