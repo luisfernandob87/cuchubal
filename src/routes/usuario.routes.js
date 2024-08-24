38566845fe4bbfd1ba490d45077f052a5603f6c3
Junior <luisfernandob87@gmail.com>
2024-08-23 19:08:03 -0600

    add features 23/08/2024

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
-const { getUsuarios, deleteUsuario, getUsuario, getUsuarioCuotas, getMisCuotas } = require('../controllers/usuarios.controller')
+const { getUsuarios, deleteUsuario, getUsuario, getUsuarioCuotas, getMisCuotas, getUsuarioCorreo } = require('../controllers/usuarios.controller')
 
 const routerUser = Router()
 
 routerUser.get('/usuarios', getUsuarios)
 routerUser.delete('/usuario/:id', deleteUsuario)
 routerUser.get('/usuario/:id', getUsuario)
+
+routerUser.get('/usuario', getUsuarioCorreo)
 
 routerUser.get('/usuario/:id/miscuotas', getMisCuotas)
 
